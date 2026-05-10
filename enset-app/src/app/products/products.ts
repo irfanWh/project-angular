0594d06ff0e240f5d550b70fa9fc62c9315d4f98
irfanWh <muhammadiw456@gmail.com>
2026-05-10 16:21:26 +0100

    condigure table de listes des products

--- enset-app/src/app/products/products.ts
+++ enset-app/src/app/products/products.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './products.css',
   standalone: true
 })
-export class Products {}
+export class Products implements OnInit {
+  products! : any;
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    this.products = [
+    {id: 1, name: "product 1", price: 100, selected: true},
+    {id: 2, name: "product 2", price: 200, selected: false},
+    {id: 3, name: "product 3", price: 300, selected: false}
+  ];
+  }
+  deleteProduct(product: any): void {
+    let conf = confirm("Are you sure?");
+    if (conf){
+      this.products = this.products.filter((p: any) => p.id != product.id);
+    }
+  }
+}
